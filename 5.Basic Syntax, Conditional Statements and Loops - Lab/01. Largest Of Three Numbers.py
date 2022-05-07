129dc9a89446372e108aa673b817dd878b817574
D-on3 <dnedyalkov396@gmail.com>
2022-05-07 12:50:39 +0300

    init

---
+++ 5.Basic Syntax, Conditional Statements and Loops - Lab/01. Largest Of Three Numbers.py
@@ -0,0 +1,10 @@
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+flist = []
+
+flist.append(first_number)
+flist.append(second_number)
+flist.append(third_number)
+print(max(flist))
