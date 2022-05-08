e2fbba96d5237904db61e246983659c61b2ebdab
D-on3 <dnedyalkov396@gmail.com>
2022-05-08 19:41:46 +0300

    init

---
+++ 8.Data Types and Variables - Lab/6.happy_new_year.py
@@ -0,0 +1,11 @@
+
+
+year= input()
+
+for index1,i in enumerate(year[0],):
+    for index2, j in enumerate(year[1], ):
+        for index3, k in enumerate(str(year[2], ):
+            for index4, l in enumerate(year[3], ):
+                if i != j and i != k and i != l and i != l and j != k and j !=l and k != l :
+
+                    print(f'{i}{j}{k}{l}')
