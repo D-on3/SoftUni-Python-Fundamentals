00cf8d57bb88c6a248f0e6271729e0d56ec8d8b3
D-on3 <dnedyalkov396@gmail.com>
2022-05-10 17:40:15 +0300

    init

---
+++ 9.Exercise Data Types and Variables/4.sum_of_chars.py
@@ -0,0 +1,13 @@
+
+number_of_letes = int(input())
+total_sum = 0
+value = 0
+for i in range(number_of_letes):
+    user_input = input()
+    value = ord(user_input)
+    total_sum += value
+
+
+
+
+print(f"The sum equals: {total_sum}")
