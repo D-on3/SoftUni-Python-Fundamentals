7b55c3e85a0b887155f99c033a6d31a19fb7b9bd
D-on3 <dnedyalkov396@gmail.com>
2022-05-13 08:56:51 +0300

    init

---
+++ 15.Exercise Functions/1.Smallest_of_Three_Numbers.py
@@ -0,0 +1,10 @@
+def smalles(arg1,arg2,arg3):
+    lisst= []
+    lisst.append(arg3)
+    lisst.append(arg2)
+    lisst.append(arg1)
+    minimal_value = min(lisst)
+    print(minimal_value)
+
+smalles(int(input()),int(input()),int(input()))
+
