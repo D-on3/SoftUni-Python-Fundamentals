e86d4d1f87920757d7c6397c07f92ccd153f670a
D-on3 <dnedyalkov396@gmail.com>
2022-05-17 11:16:01 +0300

    init

--- 11.Lists/1.Zoo.py
+++
@@ -1,6 +0,0 @@
-meercat = [input(),input(),input()]
-
-meercat[0],meercat[-1]=meercat[-1],meercat[0]
-
-print(meercat)
-
--- 11.Lists/2.Courses.py
+++
@@ -1,9 +0,0 @@
-number_of_courses = int(input())
-
-courses= []
-
-for course in range(number_of_courses):
-    courses.append(input())
-
-
-print(courses)
