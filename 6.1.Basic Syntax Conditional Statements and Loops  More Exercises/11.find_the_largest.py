3d9a038d9e404aba2b7fa19af203d25844da06a4
D-on3 <dnedyalkov396@gmail.com>
2022-05-18 11:28:53 +0300

    init

---
+++ 6.1.Basic Syntax Conditional Statements and Loops  More Exercises/11.find_the_largest.py
@@ -0,0 +1,10 @@
+
+
+num_input = input()
+new_number =  0
+while True:
+    if num_input[0] > num_input[1] and num_input[0] > num_input[2] and num_input[0] > num_input[3]:
+        num_inputp[0] = num_input[0]
+    elif num_input[1] > num_input
+    for num in range(num_input):
+        print(num)
