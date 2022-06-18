a44cc055e7f5a558b3bf9a6bff0229e5e0f55264
D-on3 <dnedyalkov396@gmail.com>
2022-06-18 13:38:12 +0300

    init
    Signed-off-by: D-on3 <dnedyalkov396@gmail.com>

---
+++ 31. Regular Expressions (RegEx)_exercises/01_capture_the_number.py
@@ -0,0 +1,14 @@
+import re
+
+
+line = input()
+pattern = r"\d+"
+numbers = []
+
+while line:
+    match = re.findall(pattern, line)
+    if match:
+        numbers.extend(match)
+    line = input()
+
+print(*numbers, end=" ")
